e8f938f525f3bc21daf1faac5a9ea385a97fc5c7
Serega <fezarikus@gmail.com>
2024-05-14 22:04:09 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/yamaps/application/App.kt
@@ -0,0 +1,13 @@
+package ru.netology.yamaps.application
+
+import android.app.Application
+import com.yandex.mapkit.MapKitFactory
+import ru.netology.yamaps.BuildConfig.MAPS_API_KEY
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        MapKitFactory.setApiKey(MAPS_API_KEY)
+    }
+}
---
+++ app/src/main/java/ru/netology/yamaps/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.netology.yamaps.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.yamaps.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
